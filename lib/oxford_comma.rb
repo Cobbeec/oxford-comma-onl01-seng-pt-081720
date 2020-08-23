f94a88dac1cb2c297ce26b787cd1fdc4425f9eba
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-23 22:56:32 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,3 +2,4 @@ def oxford_comma(array)
 ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])).to eq("kiwi, durian, starfruit, mangos, dragon fruits, lychees, and pomelos"].join(",")
 return array 
 end 
+def oxford_comma
